d23e103e33a75cf5ffd45a187a5c3540de71732b
charles-rogers <c.a.r0g3r5@gmail.com>
2019-04-03 12:57:33 -0500

    working these problems

---
+++ Algorithms/DataStructures/Linked lists/findMergePointOfTwoList.js
@@ -0,0 +1,18 @@
+function findMergeNode(headA, headB) {
+    let headA_curr = headA;
+    let headB_curr = headB;
+        while(headA_curr !== headB_curr){
+            if(headA_curr.next === null){
+                headA_curr = headB;
+            }else{
+                headA_curr = headA_curr.next;
+            }
+    
+            if(headB_curr.next == null){
+                headB_curr = headA;
+            }else{
+                headB_curr = headB_curr.next;
+            }
+        }
+    return headA_curr.data;
+    }
