c280c9f420d0404f44ade2a04efd68486f6f9655
charles-rogers <c.a.r0g3r5@gmail.com>
2019-04-30 17:34:58 -0500

    working these problems

---
+++ 30 days of code/classVsInstance.js
@@ -0,0 +1,19 @@
+function Person(initialAge) {
+    // Add some more code to run some checks on initialAge
+    if (initialAge >= 0) {
+        this.age = initialAge;
+    } else {
+        this.age = 0;
+        console.log("Age is not valid, setting age to 0.");
+    }
+    this.amIOld = function() {
+        // Do some computations in here and print out the correct statement to the console
+        if (this.age < 13) return console.log("You are young.");
+        if (this.age < 18) return console.log("You are a teenager.");
+        return console.log("You are old.");
+    };
+    this.yearPasses = function() {
+        // Increment the age of the person in here
+        this.age += 1;
+    };
+}
