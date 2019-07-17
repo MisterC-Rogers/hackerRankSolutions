2221e75e47ccecfd871c8f2b185d42bdf805fd1a
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2019-07-16 18:18:48 -0700

    working these in

---
+++ 10 days of javaScript/letAndConst.js
@@ -0,0 +1,11 @@
+function main() {
+    // Write your code here. Read input using 'readLine()' and print output using 'console.log()'.
+    const PI = Math.PI;
+    const r = parseFloat(readLine());
+    
+    // Print the area of the circle:
+    console.log((r ** 2)* PI);
+    
+    // Print the perimeter of the circle:
+    console.log(2 * r * PI);
+
