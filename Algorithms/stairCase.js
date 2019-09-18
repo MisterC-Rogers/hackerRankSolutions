03760970bceaabf72b545cbef0da63a38c487601
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2019-09-17 17:52:54 -0700

    working these in

---
+++ Algorithms/stairCase.js
@@ -0,0 +1,11 @@
+
+// Complete the staircase function below.
+function staircase(n) {
+    for (var i = n; i-- > 0;) {
+        let s = ''
+        for (let j = 0; j < n; ++j) {
+        s += j >= i ? '#' : ' '
+        }
+        console.log(s)
+    }  
+}
