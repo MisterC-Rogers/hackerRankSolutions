d2503f402d6f0b742b677f5bb1ff09cb14d37884
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2019-09-19 17:55:53 -0700

    working these in

---
+++ Algorithms/miniMaxSum.js
@@ -0,0 +1,10 @@
+// Complete the miniMaxSum function below.
+function miniMaxSum(arr) {
+
+    arr = arr.sort((a, b) => a - b)
+    let min = arr.slice(0, arr.length - 1).reduce((a, b) => a + b)
+    let max = arr.slice(1, arr.length).reduce((a, b) => a + b)
+  
+    console.log(min + ' ' + max)
+  }
+  
