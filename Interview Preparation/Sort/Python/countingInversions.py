566a3e074d0166275669c59015c047d73ede44d8
charles-rogers <c.a.r0g3r5@gmail.com>
2019-11-01 19:38:52 -0500

    working out here

--- Interview Preparation/Sort/Python/countingInversions.py
+++ Interview Preparation/Sort/Python/countingInversions.py
@@ -7,66 +7,51 @@ import re
 import sys
 
 # Complete the countInversions function below.
-def countInversions(arr):
-    res = [0] * len(arr)
+def merge(left, right):
+    counter = 0
+    result = []
+    i,j = 0,0
+    ra = result.append
+    lenl = len(left)
+    lenr = len(right)
 
-    return merge(arr, res, 0, len(arr)-1)
-
-
-def merge(arr, res, left, right):
-    if left >= right:
-        return 0
-
-    inversions = 0
-
-    left_end = (left + right) // 2
-    right_start = left_end + 1
-
-    inversions += merge(arr, res, left, left_end)
-    inversions += merge(arr, res, right_start, right)
-    inversions += mergeHalf(arr, res, left, right)
-    return inversions
-
-
-def mergeHalf(arr, res, left, right):
-    if left >= right:
-        return 0
-
-    inversions = 0
-
-    left_end = mid = (left + right) // 2
-    right_start = right_beg = left_end + 1
-    left_beg = index = left
-
-    while left <= left_end and right_start <= right:
-        pt1 = arr[left]
-        pt2 = arr[right_start]
-
-        if pt1 <= pt2:
-            res[index] = pt1
-            index += 1
-            left += 1
+# Till this condition is true, keep on appending elements into resultant array
+    while (i<lenl) and (j<lenr):
+        if left[i] <= right[j]:
+            ra(left[i]) #append ith element of left into resultant array
+            i+=1
         else:
-            res[index] = pt2
-            index += 1
-            inversions += mid - left + 1
-            right_start += 1
+            ra(right[j])  #append jth element of right into resultant array
+            j+=1
+            counter += lenl-i  #COUNT HERE!
+# it is basically specifies that if any element is remaining in the left array from -
+# ith to the last index so that it should appended into the resultant array. And similar -
+# to the right array.
+    result += left[i:]
+    result += right[j:]
+    return result, counter
 
-    while left <= left_end:
-        res[index] = arr[left]
-        left += 1
-        index += 1
+# Definition for merge sort
+# this takes an input list
 
-    while right_start <= right:
-        res[index] = arr[right_start]
-        right_start += 1
-        index += 1
+def mergesort(lst):    
+    if(len(lst)<= 1): # this means that the list is already sorted.
+        return lst, 0
+    mid = (len(lst)//2)
+# Define definition for merge which basically takes two arrays i.e., the left and right part 
+# left array will be mergesort applied over the list from starting index 
+# till the mid index
+    left, counter0 = mergesort(lst[:mid])
+# right array will be mergesort applied recursively over the list from mid index
+# till the last index 
+    right, counter1 = mergesort(lst[mid:])
+    result, counter2 = merge(left,right)
+    return result, counter2 + counter0 + counter1
 
-
-    arr[left_beg:left_end + 1] = res[left_beg:left_end + 1]
-    arr[right_beg:right + 1] = res[right_beg: right + 1]
-
-    return inversions
+def countInversions(arr):
+    #Idea: Count the number of transpositions needed when you apply the mergesort to arr.
+    final, counter = mergesort(arr)
+    return counter
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
