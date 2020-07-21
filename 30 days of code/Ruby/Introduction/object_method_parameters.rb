5cd6601153ba61cb3a14a0c10187fe56e5c59243
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2020-07-20 23:14:16 -0500

    learning ruby

--- 30 days of code/Ruby/Introduction/object_method_parameters.rb
+++ 30 days of code/Ruby/Introduction/object_method_parameters.rb
@@ -1,6 +1,10 @@
-# A method may take zero or more parameters as input. To demonstrate this, we look at the asserts we use on HackerRank. Sometimes, we have to check whether a given number a is within the range b and c (where b  c, and both inclusive ).
+# A method may take zero or more parameters as input. 
+# To demonstrate this, we look at the asserts we use on HackerRank. 
+# Sometimes, we have to check whether a given number a is within the range b and c (where b  c, and both inclusive ).
 
-# Three variables a, b, and c are already defined. Your task is to write code that checks whether a is within the range of b and c by calling the method range? (which we have defined for you as a method for this example) on a and passing b and c as arguments.
+# Three variables a, b, and c are already defined. 
+# Your task is to write code that checks whether a is within the range of b and c by calling the method range? 
+# (which we have defined for you as a method for this example) on a and passing b and c as arguments.
 
 # Hint
 
@@ -15,3 +19,13 @@
 
 # return a.range? b, c
 
+
+def range? (a = 4, b = 1, c = 5) 
+    if (b..c) === a 
+        puts true 
+    else 
+        puts false 
+    end 
+end
+
+range? 2
