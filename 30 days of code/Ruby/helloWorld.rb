295de4778e0a95be256c75e09ab0df7add9b2b37
MisterC-Rogers <c.a.r0g3r5@gmail.com>
2020-07-24 19:09:47 -0500

    learning ruby

---
+++ 30 days of code/Ruby/helloWorld.rb
@@ -0,0 +1,7 @@
+# Let's get started with Ruby!
+
+# Go ahead and type the following code in the code-editor:
+
+print "Hello HackerRank!!\n" #traditional print statement
+puts "Hello HackerRank!!" #same as the print statement though makes a new line
+p "Hello HackerRank!!" #prints whatever follows
